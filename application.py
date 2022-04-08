bc193a2205677eed84180f97a0c5479028f83fff
Niklas Hassforther <N.Hassi@gmx.net>
2022-04-08 23:18:54 +0100

    Creating the first templates and importing the needed libraries

--- application.py
+++ application.py
@@ -1,1 +1,18 @@
-import flask
+from flask import Flask, render_template, session, redirect, request, url_for, request, flash
+from flask_session import Session
+from tempfile import mkdtemp
+
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
